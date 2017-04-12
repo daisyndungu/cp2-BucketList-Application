e10554bedcd0381f0cd662abc413598f36093615
Daisy Ndungu <daisywndungu@gmail.com>
2017-04-12 19:07:30 +0300

    [ch] Test update and delete bucketlist

--- test/test.py
+++ test/test.py
@@ -49,6 +49,17 @@ class InitialTests(TestCase):
                                    data=json.dumps(new_bucketlist_name))
         self.assertEqual(response.status_code, 200)
 
+    def test_update_unexisting_bucketlist(self):
+        new_bucketlist_name = {"name": "Changed Name"}
+        response = self.client.put("/bucketlist/1",
+                                   data=json.dumps(new_bucketlist_name))
+        self.assertEqual(response.status_code, 400)
+
+    def test_delete_bucketlist(self):
+        self.client.post("/bucketlist/", data=json.dumps(self.bucketlist))
+        response = self.client.delete("/bucketlist/1")
+        self.assertEqual(response.status_code, 204)
+
 
 if __name__ == "__main__":
     unittest.main()
