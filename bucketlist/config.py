5796da628c0489ae4ee2e78cdd6cb3c19be1d816
Daisy Ndungu <daisywndungu@gmail.com>
2017-04-13 15:11:29 +0300

    [ch] Add more app configurations

--- bucketlist/config.py
+++ bucketlist/config.py
@@ -1,11 +1,32 @@
 import os
 
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-DEBUG = True
-PORT = 5432
-HOST = "127.0.0.1"
-SQLALCHEMY_ECHO = False
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", None) + "/buckectlist"
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'migrations')
+
+class Config(Object):
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    DEBUG = False
+    TEST = False
+    CSRF_ENABLED = True
+    PORT = 5432
+    HOST = "127.0.0.1"
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", None) + "/bucketlist"
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'migrations')
+
+
+class Production(Config):
+    DEBUG = False
+
+
+class Staging(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
