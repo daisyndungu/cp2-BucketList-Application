e5b6bc14e734a15c971071a0d5875aae09500c49
Daisy Ndungu <daisywndungu@gmail.com>
2017-04-18 12:31:16 +0300

    [ch 143652459] Add Create, update ande delete bucketlist item functions

--- bucketlist/view.py
+++ bucketlist/view.py
@@ -1,6 +1,6 @@
 import status
 from flask import Blueprint, request, jsonify, make_response, json
-from flask_restful import Api, Resource, marshal, fields
+from flask_restful import Api, Resource, marshal, fields, reqparse
 from flask.views import MethodView
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -24,17 +24,12 @@ class BucketlistView(Resource):
         Get one buckect list
         """
         if bucketlist_id:
-            try:
-                bucketlist_query = BucketList.query.filter_by(
-                    bucketlist_id=bucketlist_id).first()
-                if bucketlist_query:
-                    return marshal(bucketlist_query, bucketlist_format), 200
-                # return jsonify({'bucketlist': bucketlist_query})
-                else:
-                    return "bucketlist does not exist"
-            except AttributeError as e:
-                # Returns an error if bucketlist does not exist
-                return "Error: Bucketlist does not exist", 400
+            bucketlist_query = BucketList.query.filter_by(
+                bucketlist_id=bucketlist_id).first()
+            if bucketlist_query:
+                return marshal(bucketlist_query, bucketlist_format), 200
+            else:
+                return {'Error': 'bucketlist does not exist'}, 400
         else:
             bucketlist = BucketList.query.all()
             return marshal(bucketlist, bucketlist_format), 200
@@ -50,16 +45,14 @@ class BucketlistView(Resource):
         else:
             try:
                 bucketlist = BucketList(name=request_dict["name"])
-                bucketlist.add()
-                # result = json.dumps(bucketlist.get())
-                return 'Bucketlist saved successfully', 201
+                bucketlist.add(bucketlist)
+                return {'Done': 'Bucketlist saved successfully'}, 201
             except SQLAlchemyError:
                 # Returns an error if a bucketlist
                 # with the same name already exists
                 db.session.rollback()
-                response = jsonify(
-                    {"Error": " The bucketlist entered already exists"})
-                return response, 400
+
+                return {"Error": " The bucketlist entered already exists"}, 400
 
     def put(self, bucketlist_id):
         # if bucketlist_id
@@ -71,27 +64,87 @@ class BucketlistView(Resource):
                 if bucketlist_dict['name']:
                     bucketlist.name = bucketlist_dict['name']
                     bucketlist.update()
-                    return jsonify("Bucketlist Updated Successfully"), 200
+                    return {'Done': 'Bucketlist Updated Successfully'}, 200
             else:
-                return "Bucketlist does not exist", 400
+                return {'Error': 'Bucketlist does not exist'}, 400
         except:
-            return "A bucketlist with that name exists", 400
+            return {'Error': 'A bucketlist with that name exists'}, 400
 
     def delete(self, bucketlist_id):
         bucketlist = BucketList.query.filter_by(
                 bucketlist_id=bucketlist_id).first()
         if bucketlist:
-            bucketlist.delete()
-            return "Bucketlist deleted Successfully", 200
+            bucketlist.delete(bucketlist)
+            return 'Bucketlist deleted Successfully', 200
         else:
-            return "Invalid Input. Bucketlist does not exist", 400
+            return 'Invalid Input. Bucketlist does not exist', 400
 
 
-api.add_resource(BucketlistView, '/bucketlist/', endpoint="add_bucketlist")
+class BucketListItemView(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('name', type=str, location='json',
+                                   required=True, help="Item name is required")
+        self.reqparse.add_argument('description', type=str, location='json')
+        self.reqparse.add_argument('status', type=bool, location='json')
+        super(BucketListItemView, self).__init__()
+
+    def post(self, bucketlist_id):
+        """
+        Get user's input and save it to database(POST request)
+        """
+
+        bucketlist_query = BucketList.query.filter_by(
+                bucketlist_id=bucketlist_id).first()
+        if not bucketlist_query:
+            return "Invalid bucketlist", 400
+        request_dict = self.reqparse.parse_args()
+        try:
+            item = BucketListItem(name=request_dict['name'],
+                                  description=request_dict['description'],
+                                  status=request_dict['status'],
+                                  bucketlist_id=bucketlist_id)
+            item.add(item)
+            return {'Done': 'Bucketlist item saved successfully'}, 201
+        except SQLAlchemyError:
+                # Returns an error if a bucketlist item
+                # with the same name already exists
+                db.session.rollback()
+                return {"Error": " The bucketlist item entered already \
+                        exists"}, 400
+
+    def put(self, bucketlist_id, item_id):
+        bucket_list = BucketList.query.filter_by(bucketlist_id=bucketlist_id
+                                                 ).first()
+        if bucket_list:
+            bucketlist_item = BucketListItem.query.filter_by(
+                        item_id=item_id, bucketlist_id=bucketlist_id).first()
+            if not bucketlist_item:
+                return {'Error': 'Bucketlist item does not exist'}, 400
+            request = request.get_json()
+            pass
+
+    def delete(self, bucketlist_id, item_id):
+        bucket_list = BucketList.query.filter_by(bucketlist_id=bucketlist_id
+                                                 ).first()
+        if not bucket_list:
+            return {'message': 'Bucketlist does not exist'}, 400
+        bucketlist_item = BucketListItem.query.filter_by(
+                        item_id=item_id, bucketlist_id=bucketlist_id).first()
+        if not bucketlist_item:
+            return {'Error': 'Bucketlist item does not exist'}, 400
+        bucketlist_item.delete()
+        return {'Done': 'Bucketlist item deleted successfully'}, 200
+
+
+api.add_resource(BucketlistView, '/bucketlist/', endpoint='add_bucketlist')
 api.add_resource(BucketlistView, '/bucketlist/<int:bucketlist_id>',
-                 endpoint="bucketlistview")
-api.add_resource(BucketListItem, '/bucketlist/<int:bucketlist_id>/items/')
-
+                 endpoint='bucketlistview')
+api.add_resource(BucketListItemView, '/bucketlist/<int:bucketlist_id>/items/',
+                 endpoint='create_bucketlist_item')
+api.add_resource(BucketListItemView,
+                 '/bucketlist/<int:bucketlist_id>/items/<int:item_id>',
+                 endpoint='UpdateDelete_bucketlist_item')
 
 if __name__ == '__main__':
     app.run()
