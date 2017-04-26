713acab1dd9ddb3718b533d720c4f23b674da0ec
Daisy Ndungu <daisywndungu@gmail.com>
2017-04-27 00:04:52 +0300

    Runserver using manage file

--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
 import os
-from flask_script import Manager
+from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from bucketlist.models import db, app
+from bucketlist.view import *
 
-
+db.configure_mappers()
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+manager.add_command("runserver", Server())
 if __name__ == '__main__':
     manager.run()
