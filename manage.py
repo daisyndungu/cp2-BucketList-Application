80623e714fd316531e568bbce1abcbea72ec2059
Daisy Ndungu <daisywndungu@gmail.com>
2017-04-27 01:05:21 +0300

    Adds user, bucketlist and bucketlist item views

--- manage.py
+++ manage.py
@@ -4,6 +4,8 @@ from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
 from bucketlist import db, app
+from bucketlist.view import BucketListItemView, BucketlistView, UserLogin, UserRegistration
+# from bucketlist.user import 
 
 db.configure_mappers()
 migrate = Migrate(app, db)
