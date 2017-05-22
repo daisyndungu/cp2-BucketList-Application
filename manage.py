9d8cd8325030844d9b2dce85511d48ed722c2848
Daisy Ndungu <daisywndungu@gmail.com>
2017-05-22 13:51:16 +0300

    Run test using manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,6 @@
 import os
+import unittest
+import coverage
 from flask_cors import CORS
 
 from flask_script import Manager, Server
@@ -7,7 +9,7 @@ from flask_migrate import Migrate, MigrateCommand
 from bucketlist import db, create_app
 from bucketlist.view import BucketListItemView, BucketlistView
 from bucketlist.user import UserLogin, UserRegistration
-# from bucketlist import routes
+
 
 app = create_app('development')
 CORS(app)
@@ -16,5 +18,18 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 manager.add_command("runserver", Server())
+
+
+@manager.command
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('bucketlist/tests',
+                                           pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+
 if __name__ == '__main__':
     manager.run()
